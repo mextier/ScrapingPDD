a703c7ea38bd72d4cdc08b37d1e6af6125f4ee84
mextier <mextier@gmail.com>
2017-12-14 16:40:03 +0600

    1

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/
 #https://pythondigest.ru/view/17328/
 #https://habrahabr.ru/post/280238/
+
+
+
+def startScrapping():
+    pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    startScrapping()
+    
