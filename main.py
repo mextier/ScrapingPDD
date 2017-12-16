6cdc8aa0e0a05517b8dfad01948f1247001f16db
mextier <mextier@gmail.com>
2017-12-16 14:56:13 +0600

    33

--- main.py
+++ main.py
@@ -4,17 +4,24 @@
 #https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 #pip install BS4
 #pip install lxml
+#pip install request
 
 import const
 
 from urllib.request import urlopen
 from urllib.error import HTTPError
+import urllib.parse
 from bs4 import BeautifulSoup
 
 from os import system, getcwd, makedirs, listdir, remove
 from os.path import join, dirname, exists, splitext, isfile, getctime, split
 import shutil
 from datetime import datetime
+from time import sleep
+from requests import get
+
+import json
+
 
 def startScraping():
     bileti_urls = []
@@ -44,7 +51,7 @@ def startScraping():
         dirname = join(getcwd(), const.data_dir)
         try:
             shutil.rmtree(dirname)
-            removedirs(dirname)
+            #removedirs(dirname)
         except Exception as e:
             pass
         finally:
@@ -53,6 +60,8 @@ def startScraping():
     starttime = datetime.now()
     for url in bileti_urls:
         bn += 1
+        if bn ==1010:
+            continue
         try:
             html = urlopen(url)
         except HTTPError as e:
@@ -65,31 +74,61 @@ def startScraping():
             except Exception as e:
                 print("Error while parsing "+url)
             else:
-                makedirs(join(dirname,const.bilet_dir_prefix+const.bilet_dir_format.format(bn)))
-                qs = bsObj.findAll('p',{'align':'center'},{'class':'стиль10'})
-                for q in qs:
-                    if q.get_text().startswith(const.bilet_ptext):
-                        print(q.get_text())
-                        print("-"*const.string_delimiter_len)
-                        qparent = q.parent.parent.parent
-                        q1=qparent.find('span',{'class':'стиль2'})
-                        print(q1.get_text())
-                        print("-"*const.string_delimiter_len)
-                        ops = qparent.findAll('span',{'class':'стиль4'})
-                        for o in ops:
-                            print(o.get_text())
-                        a=qparent.find('button')['title']
-                        print("-"*const.string_delimiter_len)
-                        print(a)
-                        #print(q.parent.parent.parent)
-                        break
-                        #print(q.get_text())
-                        #print(q.attrs)
+                try:
+                    makedirs(join(dirname,const.bilet_dir_prefix+const.bilet_dir_format.format(bn)))
+                    qs = bsObj.findAll('p',{'align':'center'},{'class':'стиль10'})
+                    bnq = 0
+                    for q in qs:
+                        print('[{0}] {1:.0f}%'.format('#'*int((const.progress_size*bnq/len(qs))), 100*bnq/len(qs)),end='\r')
+                        questionTitle = ""
+                        questionImageUrl = ""
+                        questionOptions = []
+                        questionAnswer = 0
+                        questionExplanation = ""
+                        jd = {}
+                        if q.get_text().startswith(const.bilet_ptext):
+                            bnq += 1
+                            qparent = q.parent.parent.parent
+                            questionTitle=qparent.find('span',{'class':'стиль2'}).get_text().replace("\r","").replace("\n","").lstrip().rstrip()
+                            q=qparent.find('img')
+                            if q is not None:
+                                questionImageUrl=q['src']
+                            ops = qparent.findAll('span',{'class':'стиль4'})
+                            for o in ops:
+                                o1 = o.get_text().replace("\r","").replace("\n","")
+                                i = o1.find('.')
+                                questionOptions.append(o1[i+1:].lstrip().rstrip())
+                            a=qparent.find('button')['title']
+                            i=a.find("ответ:")
+                            questionAnswer=int(a[len("ответ:")+i:len("ответ:")+i+2])
+                            i=a.find("Комментарии:")
+                            questionExplanation=a[len("Комментарии:")+i:].replace("\r","").replace("\n","").lstrip().rstrip()
+                            jd["Title"]=questionTitle
+                            if questionImageUrl:
+                                imageURL = urllib.parse.urljoin(const.pdd_bilet_url, questionImageUrl)
+                                path = urllib.parse.urlparse(imageURL).path
+                                imageName = str(bnq)+splitext(path)[1]
+                                with open(join(dirname,const.bilet_dir_prefix+const.bilet_dir_format.format(bn),imageName), "wb") as imgfile:
+                                    response = get(imageURL)
+                                    imgfile.write(response.content)
+                                #urllib.request.urlretrieve(imageURL, join(dirname,const.bilet_dir_prefix+const.bilet_dir_format.format(bn),imageName))
+                            else:
+                                imageName=""
+                            jd["ImageName"]=imageName
+                            jd["Options"]=questionOptions
+                            jd["Answer"]=questionAnswer
+                            jd["Explanation"]=questionExplanation
+                            with open(join(dirname,const.bilet_dir_prefix+const.bilet_dir_format.format(bn),'question'+str(bnq)),"x",encoding="utf-8") as f:
+                            	json.dump(jd,f)
+                            if const.question_sleep:
+                                sleep(const.question_sleep)
 
-                break
-    print('Done in  {}'.format(datetime.now() - starttime))
-
-
+                except Exception as e:
+                    print("Ошибка: Билет:"+str(bn)+" Вопрос:"+str(bnq)+":"+str(e))
+            if const.bilet_sleep:
+                sleep(const.bilet_sleep)
+            break
+    print('\r------------------- Done in  {} -------------------'.format(datetime.now() - starttime))
 
 def startStuding():
     print("start studing")
