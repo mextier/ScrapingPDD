ee408822d3805081dc7f7f36c7d9ff9f35f778d9
mextier <mextier@gmail.com>
2017-12-23 22:53:52 +0600

    55

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#https://www.youtube.com/watch?v=KPXPr-KS-qk
 #https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/
 #https://pythondigest.ru/view/17328/
 #https://habrahabr.ru/post/280238/
